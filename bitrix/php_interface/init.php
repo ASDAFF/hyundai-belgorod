23ca49def16e21c11b2a129c0e531d87d07fa694
neeil1990 <neeil@mail.ru>
2017-03-29 11:17:18 +0300

    no message

--- bitrix/php_interface/init.php
+++ bitrix/php_interface/init.php
@@ -3,12 +3,14 @@
 
 function price_to_url($url){
     $url = str_replace('/','',$url);
-    $arSelect = Array("ID", "IBLOCK_ID", "NAME","PROPERTY_*");//IBLOCK_ID и ID обязательно должны быть указаны, см. описание arSelectFields выше
-    $arFilter = Array("IBLOCK_ID"=> 7,"CODE" => $url);
-    $res = CIBlockElement::GetList(Array(), $arFilter, false, false, $arSelect);
-    while($ob = $res->GetNextElement()){
-        $arProps = $ob->GetProperties();
-        $price = $arProps['PRICE']['VALUE'];
+    if(CModule::IncludeModule("iblock")) {
+        $arSelect = Array("ID", "IBLOCK_ID", "NAME", "PROPERTY_*");//IBLOCK_ID и ID обязательно должны быть указаны, см. описание arSelectFields выше
+        $arFilter = Array("IBLOCK_ID" => 7, "CODE" => $url);
+        $res = CIBlockElement::GetList(Array(), $arFilter, false, false, $arSelect);
+        while ($ob = $res->GetNextElement()) {
+            $arProps = $ob->GetProperties();
+            $price = $arProps['PRICE']['VALUE'];
+        }
     }
 
     return print $price;
