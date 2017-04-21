2212500ba496803219dadacda62e0b7b5f9aa9f7
neeil1990 <neeil@mail.ru>
2017-04-21 11:54:58 +0300

    no message

--- bitrix/templates/ringauto/header.php
+++ bitrix/templates/ringauto/header.php
@@ -71,7 +71,7 @@ height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
 					<li class="user-bar-nav-item"><a class="user-bar-nav-item-link showroom-open" href="#gnbShowroom">Модельный ряд</a></li>
 					<li class="user-bar-nav-item"><a class="user-bar-nav-item-link" href="/hpromise/avtomobili-s-probegom/">Автомобили с пробегом</a></li>
 					<li class="user-bar-nav-item"><a class="user-bar-nav-item-link" href="#gnbShopping">Покупка</a></li>
-					<li class="user-bar-nav-item"><a class="user-bar-nav-item-link" href="#gnbServices">Сервис</a></li>
+					<li class="user-bar-nav-item"><a class="user-bar-nav-item-link" href="/service/spetspredlozheniya/">Сервис</a></li>
 					<li class="user-bar-nav-item"><a class="user-bar-nav-item-link" href="/configurator">Конфигуратор</a></li>
 				</ul>
 			</nav>
