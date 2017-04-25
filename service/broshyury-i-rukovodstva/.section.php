3ac6d837d9c7afd6db6e7efe7d7eeb0fcb6a96a6
neeil1990 <neeil@mail.ru>
2017-04-25 15:38:30 +0300

    no message

--- service/broshyury-i-rukovodstva/.section.php
+++
@@ -1,6 +0,0 @@
-<?
-$sSectionName = "broshyury-i-rukovodstva";
-$arDirProperties = Array(
-
-);
-?>
--- service/broshyury-i-rukovodstva/index.php
+++
@@ -1,8 +0,0 @@
-<?
-require($_SERVER["DOCUMENT_ROOT"]."/bitrix/header.php");
-$APPLICATION->SetTitle("broshyury-i-rukovodstva");
-?>
-
-Text here....
-
-<?require($_SERVER["DOCUMENT_ROOT"]."/bitrix/footer.php");?>
