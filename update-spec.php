1b15636a22744f82b652e463feb9181fef0863aa
neeil1990 <neeil@mail.ru>
2017-06-02 13:46:31 +0300

    no message

--- update-spec.php
+++ update-spec.php
@@ -20,7 +20,7 @@ if(CModule::IncludeModule("iblock")):
 
 
 
-$xml = file_get_contents('http://hyundai-ringauto.ru/XML_upload_for_1c/voronezh/new_car.xml',true);
+$xml = file_get_contents($_SERVER['DOCUMENT_ROOT'].'/XML_upload_for_1c/voronezh/new_car.xml',true);
 
 
 $xml = new SimpleXMLElement($xml);
