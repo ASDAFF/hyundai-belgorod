2b5ecc3a2e87ec5f4f437e5b8ff978df9e3a69ab
neeil1990 <neeil@mail.ru>
2017-06-20 17:13:34 +0300

    no message

--- bitrix/templates/ringauto/components/bitrix/news/auto-mileage/bitrix/news.list/.default/template.php
+++ bitrix/templates/ringauto/components/bitrix/news/auto-mileage/bitrix/news.list/.default/template.php
@@ -95,6 +95,100 @@ $this->setFrameMode(true);
 			<!--Убираем
                             Убираем-->
 			<div class="content">
+
+				<div class="base__inner">
+					<div class="search-results" id="search-results">
+
+						<div class="search-results__head">
+
+							<h2>
+								Автомобили в наличии
+							</h2>
+							<div class="car-list js-adaptive-table">
+
+
+								<?foreach($arResult["ITEMS"] as $arItem):?>
+									<div class="car-list__row">
+										<div class="car-list_cell" data-label="">
+											<span class="car-list_cell-inner">
+												<i class="car-list__img-holder">
+													<a class="car-list__img-link fancycarframe" href="<?=$arItem['DETAIL_PAGE_URL']?>">
+														<img src="<?=$arItem['PROPERTIES']['SLIDER']['VALUE'][0]?>" alt="">
+													</a>
+												</i>
+
+											</span>
+										</div>
+										<div class="car-list_cell" data-label="">
+										<span class="car-list__brand">
+											<a class="fancycarframe" href="<?=$arItem['DETAIL_PAGE_URL']?>"><?echo $arItem["NAME"]?></a>
+										</span>
+
+
+					<span class="car-list_cell-inner">
+
+										<?if($arItem['PROPERTIES']['ENGINE']['VALUE']):?>
+											Двигатель: <?=$arItem['PROPERTIES']['ENGINE']['VALUE']?><br>
+										<?endif;?>
+
+						<?if($arItem['PROPERTIES']['POWER']['VALUE']):?>
+							Мощность: <?=$arItem['PROPERTIES']['POWER']['VALUE']?><br>
+						<?endif;?>
+
+						<? foreach($arItem['PROPERTIES']['STANDART']['VALUE'] as $key => $u){?>
+							<?=$u?>
+							<?
+							$key++;
+							if($arItem['PROPERTIES']['STANDART']['VALUE'][$key]){
+								print ',';
+							}
+							?>
+
+						<?}?>
+						<br>
+						<?=$arItem['PROPERTIES']['COUNT_USER']['VALUE']?>
+						<?if($arItem['PROPERTIES']['GARANTY']['VALUE'] == 'Y'):?>
+							<br>На гарантии
+						<?endif;?>
+					</span>
+
+											<br><br>
+					<span class="car-list_cell-inner car-list__city">
+						<?=$arItem['PROPERTIES']['STREET']['VALUE']?>, тел: <?=$arItem['PROPERTIES']['PHONE']['VALUE']?></span>
+										</div>
+										<div class="car-list_cell" data-label="">
+					<span class="car-list_cell-inner car-list__year">
+						<?=$arItem['PROPERTIES']['YEAR']['VALUE']?>
+					</span>
+										</div>
+										<div class="car-list_cell" data-label="">
+					<span class="car-list_cell-inner car-list__km">
+						<?=$arItem['PROPERTIES']['MILEAGE']['VALUE']?>
+					</span>
+										</div>
+										<div class="car-list_cell" data-label="">
+					<span class="car-list_cell-inner car-list__price">
+						<?=$arItem['PROPERTIES']['NEW_PRICE']['VALUE']?>    <span style="font-family: 'PT Sans';"></span>
+					</span>
+										</div>
+									</div>
+								<?endforeach;?>
+
+
+								<p class="footnote2" style="font-size: 1.2em;"></p>
+
+							</div>
+						</div>
+						<div class="base__inner">
+							<p class="footnote2">Данный раздел является информационным. Узнавайте информацию о наличии автомобилей, размещенных на сайте, у дилеров H-Promise.</p>
+						</div>
+
+
+
+					</div>
+				</div>
+
+
 				<div class="base__inner content__title">
 					<h1>Сертифицированные автомобили с пробегом</h1>
 				</div>
@@ -145,97 +239,9 @@ $this->setFrameMode(true);
 
 
 
-				<div class="base__inner">
-					<div class="search-results" id="search-results">
-
-						<div class="search-results__head">
-
-							<h2>
-								Автомобили в наличии
-							</h2>
-							<div class="car-list js-adaptive-table">
-
-
-								<?foreach($arResult["ITEMS"] as $arItem):?>
-									<div class="car-list__row">
-										<div class="car-list_cell" data-label="">
-											<span class="car-list_cell-inner">
-												<i class="car-list__img-holder">
-													<a class="car-list__img-link fancycarframe" href="<?=$arItem['DETAIL_PAGE_URL']?>">
-														<img src="<?=$arItem['PROPERTIES']['SLIDER']['VALUE'][0]?>" alt="">
-													</a>
-												</i>
-
-											</span>
-										</div>
-										<div class="car-list_cell" data-label="">
-										<span class="car-list__brand">
-											<a class="fancycarframe" href="<?=$arItem['DETAIL_PAGE_URL']?>"><?echo $arItem["NAME"]?></a>
-										</span>
-
-
-					<span class="car-list_cell-inner">
-
-										<?if($arItem['PROPERTIES']['ENGINE']['VALUE']):?>
-											Двигатель: <?=$arItem['PROPERTIES']['ENGINE']['VALUE']?><br>
-										<?endif;?>
-
-										<?if($arItem['PROPERTIES']['POWER']['VALUE']):?>
-											Мощность: <?=$arItem['PROPERTIES']['POWER']['VALUE']?><br>
-										<?endif;?>
-
-			<? foreach($arItem['PROPERTIES']['STANDART']['VALUE'] as $key => $u){?>
-							<?=$u?>
-							<?
-							$key++;
-							if($arItem['PROPERTIES']['STANDART']['VALUE'][$key]){
-								print ',';
-							}
-							?>
-
-						<?}?>
-						<br>
-						<?=$arItem['PROPERTIES']['COUNT_USER']['VALUE']?>
-						<?if($arItem['PROPERTIES']['GARANTY']['VALUE'] == 'Y'):?>
-							<br>На гарантии
-						<?endif;?>
-					</span>
-
-											<br><br>
-					<span class="car-list_cell-inner car-list__city">
-						<?=$arItem['PROPERTIES']['STREET']['VALUE']?>, тел: <?=$arItem['PROPERTIES']['PHONE']['VALUE']?></span>
-										</div>
-										<div class="car-list_cell" data-label="">
-					<span class="car-list_cell-inner car-list__year">
-						<?=$arItem['PROPERTIES']['YEAR']['VALUE']?>
-					</span>
-										</div>
-										<div class="car-list_cell" data-label="">
-					<span class="car-list_cell-inner car-list__km">
-						<?=$arItem['PROPERTIES']['MILEAGE']['VALUE']?>
-					</span>
-										</div>
-										<div class="car-list_cell" data-label="">
-					<span class="car-list_cell-inner car-list__price">
-						<?=$arItem['PROPERTIES']['NEW_PRICE']['VALUE']?>    <span style="font-family: 'PT Sans';"></span>
-					</span>
-										</div>
-									</div>
-			<?endforeach;?>
-
-
-								<p class="footnote2" style="font-size: 1.2em;"></p>
-
-							</div>
-						</div>
-						<div class="base__inner">
-							<p class="footnote2">Данный раздел является информационным. Узнавайте информацию о наличии автомобилей, размещенных на сайте, у дилеров H-Promise.</p>
-						</div>
 
 
 
-					</div>
-				</div>
 				<div class="main-section base__section">
 				</div>
 			</div>
