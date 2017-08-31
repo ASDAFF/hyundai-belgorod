3d2cf204f1d6a94bfafb05a5fa22a8e0c210176d
neeil1990 <neeil@mail.ru>
2017-08-31 14:08:06 +0300

    no message

---
+++ special-offer/index.php
@@ -0,0 +1,273 @@
+<?
+require($_SERVER["DOCUMENT_ROOT"]."/bitrix/header.php");
+$APPLICATION->SetPageProperty("title", "Hyundai Solaris купить в Воронеже. Цена на новый Хендай Солярис");
+$APPLICATION->SetTitle("NewSolaris");
+?>
+
+
+
+    <link type="text/css" href="http://www.hyundai.ru/media/css/special-offer.css" rel="stylesheet" />
+
+
+
+
+
+<div class="wrapper" id="wrapper">
+
+
+
+    <!--
+    Контент страницы начнется тут
+    -->
+
+    <main class="main" data-model="home">
+
+        <div class="so">
+            <div class="so__layout">
+                <div class="so__container so__hor-pad">
+                    <h1 class="so__title">
+                        Специальное предложение
+                    </h1>
+                    <h2 class="so__subtitle">
+                        для владельцев Hyundai
+                    </h2>
+                </div>
+            </div>
+        </div>
+        <div class="so-sec">
+            <div class="so__layout">
+                <div class="so-sec__container">
+                    <div class="so-calc so__hor-pad">
+                        <div class="so-calc__model">
+                            <h3 class="so-sec__title">
+                                Узнай свою выгоду
+                            </h3>
+                            <p class="so-calc__subtitle">
+                                Выберите модель, на которую хотите поменять свой автомобиль
+                            </p>
+                            <div class="model__selects">
+                                <select class="model__select" name="car">
+                                    <option
+                                        value="23"
+                                        data-tradein="50000"
+                                        data-programm="40000"
+                                        data-points="20000"
+                                        >
+                                        НОВЫЙ Solaris								</option>
+                                    <option
+                                        value="22"
+                                        data-tradein="25000"
+                                        data-programm="40000"
+                                        data-points="10000"
+                                        >
+                                        Creta								</option>
+                                    <option
+                                        value="7"
+                                        data-tradein="40000"
+                                        data-programm="0"
+                                        data-points="10000"
+                                        >
+                                        Elantra								</option>
+                                    <option
+                                        value="9"
+                                        data-tradein="40000"
+                                        data-programm="0"
+                                        data-points="10000"
+                                        >
+                                        i30								</option>
+                                    <option
+                                        value="6"
+                                        data-tradein="40000"
+                                        data-programm="0"
+                                        data-points="15000"
+                                        >
+                                        i40 								</option>
+                                    <option
+                                        value="18"
+                                        data-tradein="40000"
+                                        data-programm="0"
+                                        data-points="15000"
+                                        >
+                                        Tucson								</option>
+                                    <option
+                                        value="17"
+                                        data-tradein="100000"
+                                        data-programm="200000"
+                                        data-points="20000"
+                                        >
+                                        Santa Fe								</option>
+                                    <option
+                                        value="13"
+                                        data-tradein="120000"
+                                        data-programm="200000"
+                                        data-points="20000"
+                                        >
+                                        Grand Santa Fe								</option>
+                                </select>
+                            </div>
+                            <img src="/media/start/img/cars/13.png" alt="Solaris" class="so-calc__img">
+                        </div>
+                        <div class="so-calc__item">
+                            <div class="so-calc__item-title">
+                                Ваш расчет
+                            </div>
+                            <div class="so-calc__item-label so-calc__item-label_sale">
+                                <div class="so-calc__item-label-text">
+                                    Размер скидки
+                                </div>
+                            </div>
+                            <div class="so-calc__item-sum so-calc__item-sum_total so-calc__item-sum_rub">
+                                90 000
+                            </div>
+                            <div class="so-calc__item-subsum">
+                                <div class="subsum__first">
+                                    <div class="subsum__sum subsum__sum_tradein">
+                                        50 000
+                                    </div>
+                                    <div class="subsum__label">
+                                        Hyundai Trade-In
+                                    </div>
+                                </div>
+                                <div class="subsum__plus">
+                                    +
+                                </div>
+                                <div class="subsum__sec">
+                                    <div class="subsum__sum subsum__sum_programm">
+                                        40 000
+                                    </div>
+                                    <div class="subsum__label">
+                                        Гос. программа
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="so-calc__item-label so-calc__item-label_gift">
+                                <div class="so-calc__item-label-text">
+                                    Подарочные баллы в&nbspМире Хёндэ
+                                </div>
+                            </div>
+                            <div class="so-calc__item-sum so-calc__item-sum_points">
+                                20 000
+                            </div>
+                            <a class="so-calc__item-button" href="https://hpromise.hyundai.ru/trade-in/" target="_blank">
+                                условия трейд-ин
+                            </a>
+                            <div class="so-calc__item-conf-box">
+                                <a class="so-calc__item-conf" href="/configurator">
+                                    Конфигуратор
+                                    <span class="so-calc__item-conf-line"></span>
+                                </a>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="so-slider so__hor-pad">
+                        <h3 class="so-sec__title">
+                            Как воспользоваться предложением?
+                        </h3>
+                        <div class="so-slider__box">
+                            <div class="so-slider__item-box">
+                                <div class="so-slider__item so-slider__item_active">
+                                    <div class="so-slider__item-left">
+                                        <div class="so-slider__item-number">
+                                            01
+                                        </div>
+                                        <div class="so-slider__item-text">
+                                            Зарегистрируйтесь в&nbspМире Хёндэ
+                                        </div>
+                                        <a class="so-slider__item-button" href="https://mir.hyundai.ru/">
+                                            Войти в Мир
+                                        </a>
+                                    </div>
+                                    <div class="so-slider__item-right">
+                                        <div class="so-slider__item-img-box">
+                                            <img src="http://www.hyundai.ru/assets/img/special-offer/slide-1.jpg" alt="" class="so-slider__item-img">
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="so-slider__item-box">
+                                <div class="so-slider__item">
+                                    <div class="so-slider__item-left">
+                                        <div class="so-slider__item-number">
+                                            02
+                                        </div>
+                                        <div class="so-slider__item-text">
+                                            Добавьте к профилю информацию о своем текущем автомобиле
+                                        </div>
+                                    </div>
+                                    <div class="so-slider__item-right">
+                                        <div class="so-slider__item-img-box">
+                                            <img src="http://www.hyundai.ru/assets/img/special-offer/slide-2.jpg" alt="" class="so-slider__item-img">
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+
+                            <div class="so-slider__item-box">
+                                <div class="so-slider__item">
+                                    <div class="so-slider__item-left">
+                                        <div class="so-slider__item-number">
+                                            03
+                                        </div>
+                                        <div class="so-slider__item-text">
+                                            Зайдите в раздел «Витрина» и выберите вкладку «Сертификаты». Именно тут появится сертификат на выгоду по трейд-ин.
+                                        </div>
+                                    </div>
+                                    <div class="so-slider__item-right">
+                                        <div class="so-slider__item-img-box">
+                                            <img src="http://www.hyundai.ru/assets/img/special-offer/slide-3.jpg" alt="" class="so-slider__item-img">
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+
+                            <div class="so-slider__item-box">
+                                <div class="so-slider__item">
+                                    <div class="so-slider__item-left">
+                                        <div class="so-slider__item-number">
+                                            04
+                                        </div>
+                                        <div class="so-slider__item-text">
+                                            Откройте страницу сертификата, выберите модель, на которую хотите получить выгоду и нажмите кнопку «Получить».
+                                        </div>
+                                    </div>
+                                    <div class="so-slider__item-right">
+                                        <div class="so-slider__item-img-box">
+                                            <img src="http://www.hyundai.ru/assets/img/special-offer/slide-4.jpg" alt="" class="so-slider__item-img">
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="so-slider__config">
+                            <div class="so-slider__config-btn so-slider__config-btn_active so-slider__config-btn_car"></div>
+                            <div class="so-slider__config-btn"></div>
+                            <div class="so-slider__config-btn"></div>
+                            <div class="so-slider__config-btn"></div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </main>
+
+
+    <!--
+    Контент страницы закончится тут
+    -->
+
+
+
+
+</div>
+
+
+
+<script src="http://www.hyundai.ru/assets/js/common.js"></script>
+
+
+<!-- ! specific stuff for current page only ! -->
+<script type="text/javascript" src="http://www.hyundai.ru/media/js/inner.js"></script>
+<script type="text/javascript" src="special-offer.js"></script>
+
+<?require($_SERVER["DOCUMENT_ROOT"]."/bitrix/footer.php");?>
+
