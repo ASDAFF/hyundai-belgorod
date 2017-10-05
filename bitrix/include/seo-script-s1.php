537b614832bd1fa4107d7f1ffc2a0ba39a128cae
neeil1990 <neeil@mail.ru>
2017-10-05 11:17:24 +0300

    no message

--- bitrix/include/seo-script-s1.php
+++ bitrix/include/seo-script-s1.php
@@ -1,1 +1,80 @@
-11111111111111111111111111111111111111111111111111111111111111
+
+<!-- Rating@Mail.ru counter -->
+<script type="text/javascript">
+    var _tmr = window._tmr || (window._tmr = []);
+    _tmr.push({id: "2883957", type: "pageView", start: (new Date()).getTime()});
+    (function (d, w, id) {
+        if (d.getElementById(id)) return;
+        var ts = d.createElement("script"); ts.type = "text/javascript"; ts.async = true; ts.id = id;
+        ts.src = (d.location.protocol == "https:" ? "https:" : "http:") + "//top-fwz1.mail.ru/js/code.js";
+        var f = function () {var s = d.getElementsByTagName("script")[0]; s.parentNode.insertBefore(ts, s);};
+        if (w.opera == "[object Opera]") { d.addEventListener("DOMContentLoaded", f, false); } else { f(); }
+    })(document, window, "topmailru-code");
+</script><noscript><div>
+        <img src="//top-fwz1.mail.ru/counter?id=2883957;js=na" style="border:0;position:absolute;left:-9999px;" alt="" />
+    </div></noscript>
+<!-- //Rating@Mail.ru counter -->
+
+
+
+<!-- Yandex.Metrika counter --><script type="text/javascript">var yaParams = {/*Здесь параметры визита*/};</script><script type="text/javascript">(function (d, w, c) { (w[c] = w[c] || []).push(function() { try { w.yaCounter26763030 = new Ya.Metrika({id:26763030, webvisor:true, clickmap:true, trackLinks:true, accurateTrackBounce:true, trackHash:true,params:window.yaParams||{ }}); } catch(e) { } }); var n = d.getElementsByTagName("script")[0], s = d.createElement("script"), f = function () { n.parentNode.insertBefore(s, n); }; s.type = "text/javascript"; s.async = true; s.src = (d.location.protocol == "https:" ? "https:" : "http:") + "//mc.yandex.ru/metrika/watch.js"; if (w.opera == "[object Opera]") { d.addEventListener("DOMContentLoaded", f, false); } else { f(); } })(document, window, "yandex_metrika_callbacks");</script><noscript><div><img src="//mc.yandex.ru/watch/26763030" style="position:absolute; left:-9999px;" alt="" /></div></noscript><!-- /Yandex.Metrika counter -->
+
+
+<!-- StreamWood code -->
+<link href="https://clients.streamwood.ru/StreamWood/sw.css" rel="stylesheet" type="text/css" />
+<script type="text/javascript" src="https://clients.streamwood.ru/StreamWood/sw.js" charset="utf-8"></script>
+<script type="text/javascript">
+    swQ(document).ready(function(){
+        swQ().SW({
+            swKey: '76ff58d142adc795d56c0d51224093fd',
+            swDomainKey: '3b1971e77373033637f5152adf917bfc'
+        });
+        swQ('body').SW('load');
+    });
+</script>
+<!-- /StreamWood code -->
+
+
+<script type="text/javascript">
+    /* <![CDATA[ */
+    var google_conversion_id = 971037742;
+    var google_custom_params = window.google_tag_params;
+    var google_remarketing_only = true;
+    /* ]]> */
+</script>
+<script type="text/javascript" src="//www.googleadservices.com/pagead/conversion.js">
+</script>
+<noscript>
+    <div style="display:inline;">
+        <img height="1" width="1" style="border-style:none;" alt="" src="//googleads.g.doubleclick.net/pagead/viewthroughconversion/971037742/?value=0&amp;guid=ON&amp;script=0"/>
+    </div>
+</noscript>
+<div id="carfinPrice"><!-- --></div>
+<script type="text/javascript">
+    (function() {
+        var carfinParam = {
+            'partner': 402,
+            'htmlPrice': 'carfinPrice',
+            'positionAlign': 'left',
+            'positionTop': 50,
+            'tpl': 14
+        };
+        var carfinScript = document.createElement('script');
+        carfinScript.type = 'text/javascript';
+        carfinScript.async = true;
+        carfinScript.charset = 'utf-8';
+        carfinScript.src = (("https:" == document.location.protocol) ? "https://" : "http://") + 'car-fin.ru/widget/price.js';
+        var carfinScriptDone = false;
+        carfinScript.onload = carfinScript.onreadystatechange = function() {
+            if (!carfinScriptDone
+                && (!this.readyState || this.readyState == "loaded" || this.readyState == "complete")
+                && typeof(carfinCalculator) == 'object') {
+                carfinScriptDone = true;
+                carfinCalculator.run(carfinParam);
+            }
+        }
+        var firstScript = document.getElementsByTagName('script')[0];
+        firstScript.parentNode.insertBefore(carfinScript, firstScript);
+    })();
+</script>
+<script type="text/javascript" src="//cdn.perezvoni.com/widget/js/przv.js?przv_code=4116-773dfffea-f2c47-c4713a9-47826773dfffea-26773dfffea" ></script>
--- bitrix/include/seo-script-s2.php
+++ bitrix/include/seo-script-s2.php
@@ -1,1 +1,83 @@
-222222222222222222222222222222222222222222
+
+<!-- Rating@Mail.ru counter -->
+<script type="text/javascript">
+    var _tmr = window._tmr || (window._tmr = []);
+    _tmr.push({id: "2883957", type: "pageView", start: (new Date()).getTime()});
+    (function (d, w, id) {
+        if (d.getElementById(id)) return;
+        var ts = d.createElement("script"); ts.type = "text/javascript"; ts.async = true; ts.id = id;
+        ts.src = (d.location.protocol == "https:" ? "https:" : "http:") + "//top-fwz1.mail.ru/js/code.js";
+        var f = function () {var s = d.getElementsByTagName("script")[0]; s.parentNode.insertBefore(ts, s);};
+        if (w.opera == "[object Opera]") { d.addEventListener("DOMContentLoaded", f, false); } else { f(); }
+    })(document, window, "topmailru-code");
+</script><noscript><div>
+        <img src="//top-fwz1.mail.ru/counter?id=2883957;js=na" style="border:0;position:absolute;left:-9999px;" alt="" />
+    </div></noscript>
+<!-- //Rating@Mail.ru counter -->
+
+<!-- perezvoni.com для Липецка -->
+<script type="text/javascript" src="//cdn.perezvoni.com/widget/js/przv.js?przv_code=4116-f88e0a-e569087d67698b-48ce7-698b72ece94f88e0a-2e56" ></script>
+
+
+<!-- StreamWood code для Липецка -->
+<link href="https://clients.streamwood.ru/StreamWood/sw.css" rel="stylesheet" type="text/css" />
+<script type="text/javascript" src="https://clients.streamwood.ru/StreamWood/sw.js" charset="utf-8"></script>
+<script type="text/javascript">
+    swQ(document).ready(function(){
+        swQ().SW({
+            swKey: '76ff58d142adc795d56c0d51224093fd',
+            swDomainKey: '18501211c303f09a24cd065c71514778'
+        });
+        swQ('body').SW('load');
+    });
+</script>
+<!-- /StreamWood code -->
+
+<!-- Yandex.Metrika counter для Липецка -->
+<script type="text/javascript" >
+    (function (d, w, c) {
+        (w[c] = w[c] || []).push(function() {
+            try {
+                w.yaCounter45652815 = new Ya.Metrika({
+                    id:45652815,
+                    clickmap:true,
+                    trackLinks:true,
+                    accurateTrackBounce:true,
+                    webvisor:true
+                });
+            } catch(e) { }
+        });
+
+        var n = d.getElementsByTagName("script")[0],
+            s = d.createElement("script"),
+            f = function () { n.parentNode.insertBefore(s, n); };
+        s.type = "text/javascript";
+        s.async = true;
+        s.src = "https://mc.yandex.ru/metrika/watch.js";
+
+        if (w.opera == "[object Opera]") {
+            d.addEventListener("DOMContentLoaded", f, false);
+        } else { f(); }
+    })(document, window, "yandex_metrika_callbacks");
+</script>
+<noscript><div><img src="https://mc.yandex.ru/watch/45652815" style="position:absolute; left:-9999px;" alt="" /></div></noscript>
+<!-- /Yandex.Metrika counter -->
+
+<!-- счетчик мейл he для липецка-->
+<!-- Rating@Mail.ru counter -->
+<script type="text/javascript">
+    var _tmr = window._tmr || (window._tmr = []);
+    _tmr.push({id: "2925919", type: "pageView", start: (new Date()).getTime()});
+    (function (d, w, id) {
+        if (d.getElementById(id)) return;
+        var ts = d.createElement("script"); ts.type = "text/javascript"; ts.async = true; ts.id = id;
+        ts.src = (d.location.protocol == "https:" ? "https:" : "http:") + "//top-fwz1.mail.ru/js/code.js";
+        var f = function () {var s = d.getElementsByTagName("script")[0]; s.parentNode.insertBefore(ts, s);};
+        if (w.opera == "[object Opera]") { d.addEventListener("DOMContentLoaded", f, false); } else { f(); }
+    })(document, window, "topmailru-code");
+</script><noscript><div>
+        <img src="//top-fwz1.mail.ru/counter?id=2925919;js=na" style="border:0;position:absolute;left:-9999px;" alt="" />
+    </div></noscript>
+<!-- //Rating@Mail.ru counter -->
+
+<!-- мейл ру-->
