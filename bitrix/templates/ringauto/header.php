65fa905c33744ac19c7da29fd03afdf78129b934
neeil1990 <neeil@mail.ru>
2017-10-20 15:28:04 +0300

    all

--- bitrix/templates/ringauto/header.php
+++ bitrix/templates/ringauto/header.php
@@ -84,6 +84,10 @@
 				<span class="navbar-primary__sp-title">«Ринг Авто» г. Воронеж, ул. Изыскателей, 39, корп. 2</span>
 				<span class="navbar-primary__sp-phone-w">
 					<span class="navbar-primary__sp-phone">+7 473 261-7777</span>
+				</span><br>
+				<span class="navbar-primary__sp-title">«Ринг Авто Север» г. Воронеж, 7 км автодороги Воронеж-Москва, напротив Сити-парка «Град»</span>
+				<span class="navbar-primary__sp-phone-w">
+					<span class="navbar-primary__sp-phone">+7 473 347-7777</span>
 				</span>
 
 			</div>
