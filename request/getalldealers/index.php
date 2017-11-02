73bf206295f6af7206096bb82cce1b5371ef90b8
neeil1990 <neeil@mail.ru>
2017-11-02 15:04:30 +0300

    no message

---
+++ request/getalldealers/index.php
@@ -0,0 +1,27 @@
+
+
+<?
+
+$mapArr = array(
+    array(
+        "address" => "394002, г. Воронеж, улица Изыскателей, 39 корпус 2",
+        "city_id" => "19",
+        "code" => "C40AF31090",
+        "email" => "",
+        "encode_phone" => "",
+        "id" => "217",
+        "latitude" => "51.6696562",
+        "longitude" => "39.2993585",
+        "name" => "",
+        "phone" => "",
+        "site" => "",
+        "special" => "1",
+        "wa" => "0"
+    )
+);
+
+
+
+
+
+echo json_encode($mapArr);
