20f39c6875c83c23698c0159278d0537217111ff
neeil1990 <neeil@mail.ru>
2017-11-21 12:45:33 +0300

    no message

--- bitrix/templates/.default/components/bitrix/news/auto-mileage/bitrix/news.list/.default/result_modifier.php
+++ bitrix/templates/.default/components/bitrix/news/auto-mileage/bitrix/news.list/.default/result_modifier.php
@@ -2,7 +2,7 @@
 
 
 foreach($arResult["ITEMS"] as $key => &$arItem){
-    if(stristr($email, 'preview')){
+    if(stristr($arItem['PROPERTIES']['SLIDER']['VALUE'], 'preview')){
         $arResult["ITEMS"][$key]['PROPERTIES']['SLIDER']['VALUE'][0] = $arItem['PROPERTIES']['SLIDER']['VALUE'];
     }else{
         unset($arResult["ITEMS"][$key]);
