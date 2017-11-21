7e1457c17b0c6c6829ad2cd1ffef7c4ced167ab7
neeil1990 <neeil@mail.ru>
2017-11-21 12:46:54 +0300

    no message

--- bitrix/templates/.default/components/bitrix/news/auto-mileage/bitrix/news.list/.default/result_modifier.php
+++ bitrix/templates/.default/components/bitrix/news/auto-mileage/bitrix/news.list/.default/result_modifier.php
@@ -1,7 +1,7 @@
 <?php
 
 
-foreach($arResult["ITEMS"] as $key => &$arItem){
+foreach($arResult["ITEMS"] as $key => $arItem){
     if(stristr($arItem['PROPERTIES']['SLIDER']['VALUE'], 'preview')){
         $arResult["ITEMS"][$key]['PROPERTIES']['SLIDER']['VALUE'][0] = $arItem['PROPERTIES']['SLIDER']['VALUE'];
     }else{
