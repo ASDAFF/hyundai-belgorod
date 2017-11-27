80bd6a645ad03b694f56ba27cfe7a60144621722
neeil1990 <neeil@mail.ru>
2017-11-27 11:06:28 +0300

    no message

--- bitrix/templates/.default/components/bitrix/catalog.section.list/menu/template.php
+++ bitrix/templates/.default/components/bitrix/catalog.section.list/menu/template.php
@@ -55,7 +55,7 @@ $arSectionDeleteParams = array("CONFIRM" => GetMessage('CT_BCSL_ELEMENT_DELETE_C
 		<div class="showroom__cat-items">
 
 			<?
-			$arSelect = Array("ID", "IBLOCK_ID","CODE","PREVIEW_PICTURE", "NAME","PROPERTY_PRICE","PROPERTY_PROFIT_MENU");
+			$arSelect = Array("ID", "IBLOCK_ID","CODE","PREVIEW_PICTURE", "NAME","PROPERTY_PRICE","PROPERTY_PROMO");
 			$arFilter = Array("IBLOCK_ID" => $arSection['IBLOCK_ID'],"SECTION_ID" => $arSection['ID'],"ACTIVE"=>"Y");
 			$res = CIBlockElement::GetList(Array(), $arFilter, false, Array(), $arSelect);
 			while($ob = $res->GetNextElement()):
@@ -63,10 +63,10 @@ $arSectionDeleteParams = array("CONFIRM" => GetMessage('CT_BCSL_ELEMENT_DELETE_C
 			?>
 
 			<!--showroom__cat-item--promo-->
-			<a href="/<?=$arFields['CODE']?>" class="showroom__cat-item" title="<?=$arFields['NAME']?>">
+			<a href="/<?=$arFields['CODE']?>" class="showroom__cat-item <?if($arFields['PROPERTY_PROMO_VALUE']):?>showroom__cat-item--promo<?endif;?>" title="<?=$arFields['NAME']?>">
 				<span class="showroom__cat-item-name"><?=$arFields['NAME']?></span>
 				<span class="showroom__cat-item-price"><?=str_replace(array('руб.','руб'),'',$arFields['PROPERTY_PRICE_VALUE'])?> ₽</span>
-				<span class="showroom__cat-item-price"><?=$arFields['PROPERTY_PROFIT_MENU_VALUE']?></span>
+<!--				<span class="showroom__cat-item-price">--><?//=$arFields['PROPERTY_PROFIT_MENU_VALUE']?><!--</span>-->
 				<img src="<?=CFile::GetPath($arFields["PREVIEW_PICTURE"]);?>" class="showroom__cat-item-image" alt="Новый Solaris"/>
 			</a>
 
