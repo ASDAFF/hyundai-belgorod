355c5874b5bbd4b1e22731ddb9291df88795f9a2
neeil1990 <neeil@mail.ru>
2018-01-11 15:57:31 +0300

    seo

--- bitrix/include/seo-script-s4.php
+++ bitrix/include/seo-script-s4.php
@@ -1,3 +1,17 @@
+<!-- StreamWood code по просьбе маркетолога от 11.01.2018 (Виктория) -->
+<link href="https://clients.streamwood.ru/StreamWood/sw.css" rel="stylesheet" type="text/css" />
+<script type="text/javascript" src="https://clients.streamwood.ru/StreamWood/sw.js" charset="utf-8"></script>
+<script type="text/javascript">
+  swQ(document).ready(function(){
+    swQ().SW({
+      swKey: '76ff58d142adc795d56c0d51224093fd',
+      swDomainKey: 'a8d7b755861ad04dcb98a480b7b4e958'
+    });
+    swQ('body').SW('load');
+  });
+</script>
+<!-- /StreamWood code -->
+
 <script type="text/javascript" src="//cdn.perezvoni.com/widget/js/przv.js?przv_code=4116-a213b-b9123-e52ff97b9123-3bc5bd61-238de52ff97b9123" ></script>
 
 
