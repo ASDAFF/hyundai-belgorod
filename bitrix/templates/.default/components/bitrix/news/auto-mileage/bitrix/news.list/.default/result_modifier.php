55148c230f604584fafdac8b24f7186032e3c6fd
neeil1990 <neeil@mail.ru>
2018-01-25 17:09:11 +0300

    no message

--- bitrix/templates/.default/components/bitrix/news/auto-mileage/bitrix/news.list/.default/result_modifier.php
+++ bitrix/templates/.default/components/bitrix/news/auto-mileage/bitrix/news.list/.default/result_modifier.php
@@ -3,13 +3,13 @@
 
 foreach($arResult["ITEMS"] as $key => $arItem){
 
-    if(stristr(implode(',',$arItem['PROPERTIES']['SLIDER']['VALUE']), 'preview') === false){
+    if(stristr(implode(',',$arItem['PROPERTIES']['SLIDER']['VALUE']), '_photo1') === false){
         unset($arResult["ITEMS"][$key]);
     }
 
     foreach($arItem['PROPERTIES']['SLIDER']['VALUE'] as $prw){
 
-        if(stristr($prw, 'preview')){
+        if(stristr($prw, '_photo1')){
             $arResult["ITEMS"][$key]['PROPERTIES']['SLIDER']['VALUE'][0] = $prw;
         }
 
