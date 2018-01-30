b732417f7bf23ee8d52122d0cfa4e27378e22ddd
neeil1990 <neeil@mail.ru>
2018-01-30 18:30:59 +0300

    no message

--- bitrix/templates/.default/components/bitrix/news.detail/promo.new/template.php
+++ bitrix/templates/.default/components/bitrix/news.detail/promo.new/template.php
@@ -409,6 +409,18 @@ $this->setFrameMode(true);
 	});
 
 </script>
+
+<?$APPLICATION->IncludeComponent(
+	"bitrix:main.include",
+	"",
+	Array(
+		"AREA_FILE_SHOW" => "file",
+		"AREA_FILE_SUFFIX" => "inc",
+		"EDIT_TEMPLATE" => "",
+		"PATH" => "/bitrix/include/seo-script-".SITE_ID.".php"
+	)
+);?>
+
 </body>
 </html>
 
