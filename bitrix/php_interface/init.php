14b5cef3f26239e8ed4017f777ddd3be28ba2db7
neeil1990 <neeil@mail.ru>
2018-03-07 15:00:13 +0300

    no message

--- bitrix/php_interface/init.php
+++ bitrix/php_interface/init.php
@@ -208,8 +208,8 @@ function offers_filter_promo($url){
         $id_spec = 25;
     }elseif($_SERVER['HTTP_HOST'] == 'hyundai-oskol.ru'){
         $id_mod = 36;
-        //$id_spec = 35;
-        $id_spec = 8;
+        $id_spec = 35;
+        //$id_spec = 8;
     }elseif($_SERVER['HTTP_HOST'] == 'hyundai-ringsever.ru'){
         $id_mod = 45;
         //$id_spec = 47;
