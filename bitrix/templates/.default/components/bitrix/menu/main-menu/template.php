248315f3cbd2e953e460691fec42dcdc3c98b282
neeil1990 <neeil@mail.ru>
2018-03-16 10:43:49 +0300

    no message

--- bitrix/templates/.default/components/bitrix/menu/main-menu/template.php
+++ bitrix/templates/.default/components/bitrix/menu/main-menu/template.php
@@ -25,7 +25,7 @@
 						<span class="main-nav__link" data-role="showroom-submenu-link">Модельный ряд</span>
 					</li>
 
-					<? if(SITE_ID != "s3"): ?>
+					<? if(SITE_ID == "s5"): ?>
 					<li class="main-nav__item">
 						<a href="/offer/" class="main-nav__link">Авто в наличии</a>
 					</li>
