999b9af915df1336e035e6bb46b039d3b0ba0109
neeil1990 <neeil@mail.ru>
2018-06-06 13:57:44 +0300

    srv

--- promo/grand_santa_fe_special_edition/index.php
+++ promo/grand_santa_fe_special_edition/index.php
@@ -1,9 +1,7 @@
 <?
 require($_SERVER["DOCUMENT_ROOT"]."/bitrix/header.php");
 $APPLICATION->SetTitle("Hyundai Grand Santa Fe Special Edition");
-?>
-
-    <!--
+?><!--
     Контент страницы начнется тут
     -->
 
@@ -22,7 +20,7 @@ $APPLICATION->SetTitle("Hyundai Grand Santa Fe Special Edition");
                 <!-- WARNING -->
                 <!-- this checks only img src!! no checking by offer ID -->
 
-                <img class="promo-head" src="http://www.hyundai.ru/media/offers_image/a2d0c088518848556962343f1a54f034_Grand_Santa_fe_SE_banner_promo.jpg" style=""/>
+                <img src="http://www.hyundai.ru/media/offers_image/a2d0c088518848556962343f1a54f034_Grand_Santa_fe_SE_banner_promo.jpg" class="promo-head">
 
                 <!-- old syntax: -->
                 <!-- <img class="promo-head" src="http://www.hyundai.ru/media/offers_image/2f94f767b5fe67aa50652b8e29c26b52_Grand_Santa_fe_SE_banner_preview.jpg" style=""/> -->
@@ -39,13 +37,13 @@ $APPLICATION->SetTitle("Hyundai Grand Santa Fe Special Edition");
                                 <!--<h2 class="title-offer__lev02 multiple-cars">Hyundai Grand Santa Fe Special Edition</h2>-->
                                 <div class="row-inline__cell">
                                     <strong>От:</strong>
-                                    <div class="offer-price-count ruble">2 499 000</div>
+                                    <div class="offer-price-count ruble">2 614 000</div>
                                 </div>
                             </div>
 
                             <div class="row-inline">
                                 <p class="offer-terms__text">
-                                    Рады представить вам лимитированную серию Hyundai Grand Santa Fe. Модель оснащена уникальным набором опций и доступна с новым цветовым решением отделки салона. Максимальная рекомендованная розничная цена комплектации Grand Santa Fe Limited Edition - 2 499 000 рублей.<br><br>
+                                    Рады представить вам лимитированную серию Hyundai Grand Santa Fe. Модель оснащена уникальным набором опций и доступна с новым цветовым решением отделки салона. Максимальная рекомендованная розничная цена комплектации Grand Santa Fe Limited Edition - 2 614 000 рублей.<br><br>
                                     Автомобили лимитированной серии агрегатируются 2.2-литровым дизельным мотором мощностью 200 л.с., 6-ступенчатой автоматической трансмиссией и полным приводом.<br><br>
                                     Автомобили Grand Santa Fe лимитированной серии оборудованы специальным сетом опций, которые обеспечат водителю высокую безопасность и комфорт. 7 подушек безопасности, включая коленную, значительно снижают вероятность получения травм в случае ДТП. Встроенная навигация и премиальный звук от Infinity делают использование мультимедийной системой удобным и приятным. Электронный стояночный тормоз с режимом автоматического удержания (Autohold) значительно улучшает комфорт эксплуатации автомобиля в условиях пробок. Бесключевой доступ и кнопка Start/Stop обеспечивают удобное открытие и запуск автомобиля.<br><br>
                                     Кроме того, автомобили Grand Santa Fe Limited Edition оборудованы двумя высокотехнологичными функциями - системой автоматической парковки, которая значительно увеличивает удобство эксплуатации в условиях загруженных улиц городов, и панорамной крышей с люком.  Большой панорамный люк с автоматической шторкой делает салон еще более комфортным и просторным, заполняя его светом и воздухом.
@@ -55,7 +53,7 @@ $APPLICATION->SetTitle("Hyundai Grand Santa Fe Special Edition");
 
                             <div class="offer-actions clearfix">
                                 <div class="row row-left">
-                                    <img src="http://www.hyundai.ru/media/offers_image/d26775bde21cc3d21646f9833a794cb6_NCe_Mystic_Beige_1024_11.jpg" alt="Hyundai Santa fe" width="440" height="180">
+                                    <img width="440" alt="Hyundai Santa fe" src="http://www.hyundai.ru/media/offers_image/d26775bde21cc3d21646f9833a794cb6_NCe_Mystic_Beige_1024_11.jpg" height="180">
                                 </div>
                                 <div class="row row-right">
                                     <div class="offer-actions__button-span">
@@ -63,7 +61,7 @@ $APPLICATION->SetTitle("Hyundai Grand Santa Fe Special Edition");
 
 
 
-                                        <div id="brochure_requester"><a data-gtm-havas-model="santafe" href="#" data-model="Grand Santa Fe" for="brochures_request"  target="_blank" class="zing_form_toggler offer-actions__button brosh">Брошюра</a></div>
+                                        <div id="brochure_requester"><a data-gtm-havas-model="santafe" href="#" data-model="Grand Santa Fe" for="brochures_request" target="_blank" class="zing_form_toggler offer-actions__button brosh">Брошюра</a></div>
                                     </div>
 
 
@@ -78,7 +76,7 @@ $APPLICATION->SetTitle("Hyundai Grand Santa Fe Special Edition");
                                     <a href="http://www.hyundai.ru/configurator/car/13/modification" target="_blank" class="offer-actions__button credit">Кредит</a>
                                     <a href="javascript:void(0);" class="offer-actions__button testdrive zing_form_toggler_spec" for="testdrive">Тест-драйв</a>
                                 </div>
-                                <a class="offer-actions__description" href="/GrandSantaFe">Описание модели ></a>
+                                <a class="offer-actions__description" href="/GrandSantaFe">Описание модели &gt;</a>
                             </div>
 
                             <!-- additional terms as footer -->
@@ -101,7 +99,7 @@ $APPLICATION->SetTitle("Hyundai Grand Santa Fe Special Edition");
                 </div>
             </div>
         </div>
-        </div>
+        
 
         <!--RuTarget-->
         <script type="text/javascript">
@@ -117,8 +115,4 @@ $APPLICATION->SetTitle("Hyundai Grand Santa Fe Special Edition");
 
     <!--
     Контент страницы закончится тут
-    -->
-
-
-
-<?require($_SERVER["DOCUMENT_ROOT"]."/bitrix/footer.php");?>
+    --><?require($_SERVER["DOCUMENT_ROOT"]."/bitrix/footer.php");?>
