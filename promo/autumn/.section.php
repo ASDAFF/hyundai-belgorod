2fad5b2d724195ed586e53be7da925d626a5786e
neeil1990 <neeil@mail.ru>
2018-09-05 17:42:10 +0300

    no message

---
+++ promo/autumn/.section.php
@@ -0,0 +1,6 @@
+<?
+$sSectionName = "autumn";
+$arDirProperties = Array(
+
+);
+?>
---
+++ promo/autumn/index.php
@@ -0,0 +1,109 @@
+<?
+require($_SERVER["DOCUMENT_ROOT"]."/bitrix/header.php");
+$APPLICATION->SetTitle("Бесплатная диагностика + 1 литр масла в подарок - cервисная программа «Лучшее для своих»");
+?>
+
+	<!--
+    Контент страницы начнется тут
+    -->
+
+	<main class="main" data-model="home">
+
+		<style>
+			#wrapper {
+				background: #f5f5f5 !important;
+			}
+		</style>
+
+		<div class="offers-container">
+			<div class="content_area">
+				<!-- hide elantra and ix35 banner-->
+
+				<!-- WARNING -->
+				<!-- this checks only img src!! no checking by offer ID -->
+
+				<img class="promo-head" src="img/Hyundai_Shell_aug_1920x530.jpg" style=""/>
+
+				<!-- old syntax: -->
+				<!-- <img class="promo-head" src="http://www.hyundai.ru/media/offers_image/766be949ba7a5f92fc74d6e23aca1ed7_Hyundai_Shell_aug_1500x545.jpg" style=""/> -->
+
+				<!-- end of hiding banner-->
+
+				<div class="offers-content">
+
+					<h1 class="title-offer bordr-btm">Бесплатная диагностика + 1 литр масла в подарок - cервисная программа «Лучшее для своих»</h1>
+
+					<div class="offers-block">
+
+						<div class="offer-terms">
+							<p class="offer-terms__text">
+								Мы предлагаем вам стать участником программы «Лучшее для своих» и пройти <b>бесплатную диагностику*</b> вашего автомобиля, а также получить подарок при прохождении ТО или масляного сервиса - <b>1 литр масла Shell Helix Ultra**.</b></p><br/>
+							<a href="/service/service-request/" class="request-service-btn">записаться на сервис</a><br/><br/>
+							<p class="offer-terms__text">Предложение действует для всего модельного ряда Hyundai старше 3 лет с момента выпуска автомобиля.</p>
+							<p class="offer-terms__text">Подробности кампании Вы можете уточнить у <a href="/contacts/" target="_blank">официальных дилеров Hyundai.</a></p>
+
+
+
+						</div>
+						<div class="tip-supscript bordr-btm">
+							<p style="font-size:10px"><sup>*</sup>Сервисная программа «Лучшее для своих» включает в себя комплексную бесплатную диагностику автомобилей Hyundai старше 2-х лет с момента их выпуска по 36 ключевым пунктам без ограничений по сроку эксплуатации и пробегу автомобилей.
+							</p>
+							<p style="font-size:10px"><sup>**</sup> клиент может получить бесплатно не более 1 литра масла Shell Helix Ultra ECT AH 5W-30 Shell Helix Ultra 0W 40 или Shell Helix Ultra 0W 30 исходя из общего рекомендованного в сервисной книжке объема только во время проведения замены прежнего масла в автомобиле на полученный в рамках предложения Shell Helix Ultra ECT AH 5W-30 Shell Helix Ultra 0W 40 или Shell Helix Ultra 0W 30. Лично в руки масло Shell Helix Ultra ECT AH 5W-30 Shell Helix Ultra 0W 40 или Shell Helix Ultra 0W 30не выдается.</p>
+
+						</div>
+
+
+
+					</div>
+
+				</div>
+
+			</div>
+		</div>
+
+
+		<style>
+
+			.request-service-btn {
+				-moz-user-select: none;
+				background: #1d88ce none repeat scroll 0 0;
+				border: 2px solid #fff;
+				color: #fff !important;
+				display: inline-block;
+				font-family: "Arial",sans-serif;
+				font-size: 13px;
+				font-weight: bold;
+				padding: 16px 29px;
+				text-align: center;
+				text-transform: uppercase;
+				transition: background 0.15s cubic-bezier(0.18, 0.89, 0.32, 1.28) 0s, border-color 0.15s cubic-bezier(0.18, 0.89, 0.32, 1.28) 0s, color 0.15s cubic-bezier(0.18, 0.89, 0.32, 1.28) 0s;
+				text-decoration: none !important;
+			}
+			.request-service-btn:hover {
+				background: rgba(29, 136, 206, 0.8) none repeat scroll 0 0;
+				color: #1d88ce !mportant;
+				text-decoration: none;
+				border: 2px solid #fff;
+			}
+
+		</style>
+		</div>
+		</div>
+
+		<!--RuTarget-->
+		<script type="text/javascript">
+			var _rutarget=window._rutarget||[];
+			_rutarget.push({
+				'event':'otherPage',
+				'nosync': 'true'
+			});
+		</script>
+		<!--/RuTarget-->
+	</main>
+
+
+	<!--
+    Контент страницы закончится тут
+    -->
+
+<?require($_SERVER["DOCUMENT_ROOT"]."/bitrix/footer.php");?>
